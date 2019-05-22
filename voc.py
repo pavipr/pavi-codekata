433e2fd9cea792994b5d46609503a2868a1e4318
pavipr <50615825+pavipr@users.noreply.github.com>
2019-05-22 12:27:51 +0530

    Add files via upload

---
+++ voc.py
@@ -0,0 +1,9 @@
+
+ch=input(" enter")
+if(ch>='a' and ch<='z' or ch>='A' and ch<='Z' ):
+  if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch =='O' or ch == 'U') :
+    print("vowel")
+  else:
+    print("consonant")
+else:
+  print("invalid")
