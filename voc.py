d5eab01949fa0fdf3ba7a8008fc129281702cca2
pavipr <50615825+pavipr@users.noreply.github.com>
2019-05-22 12:32:17 +0530

    Update voc.py

--- voc.py
+++ voc.py
@@ -1,5 +1,5 @@
 
-ch=input(" enter")
+ch=input("")
 if(ch>='a' and ch<='z' or ch>='A' and ch<='Z' ):
   if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch =='O' or ch == 'U') :
     print("vowel")
