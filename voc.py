6b0e44aec058f5a0595686f5db6d4168582d7829
pavipr <50615825+pavipr@users.noreply.github.com>
2019-05-22 12:34:50 +0530

    Update voc.py

--- voc.py
+++ voc.py
@@ -2,8 +2,8 @@
 ch=input("")
 if(ch>='a' and ch<='z' or ch>='A' and ch<='Z' ):
   if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch =='O' or ch == 'U') :
-    print("vowel")
+    print("Vowel")
   else:
-    print("consonant")
+    print("Consonant")
 else:
   print("invalid")
