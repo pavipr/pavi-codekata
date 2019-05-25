5a6346001d2b2e44f4b086a1f479b0050a3776a3
pavipr <50615825+pavipr@users.noreply.github.com>
2019-05-25 10:43:45 +0530

    Update priin.py

--- priin.py
+++ priin.py
@@ -1,7 +1,7 @@
 n=int(input("enter"))
 q=int(input("enter"))
 if(n,q<=100000):
-  for num in range(n,q+1):
+  for num in range(n,q):
      if(num>1):
       for i in range(2,num):
         if(num%i)==0:
